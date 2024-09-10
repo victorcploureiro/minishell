b5d5379d438e926ed045b3509e2c6af13b4c63f6
Victor Carrara Polido Loureiro <vcarrara@c2r2p4.42sp.org.br>
2024-09-10 16:29:38 -0300

    commit 0 - project start

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 16:18:37 by vcarrara          #+#    #+#             */
+/*   Updated: 2024/09/10 16:23:44 by vcarrara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <errno.h>
+# include <dirent.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
