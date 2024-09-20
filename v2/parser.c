5d25f00ec8a19b6cadd0fbb27dfbd5c16161541f
Victor Carrara Polido Loureiro <vcarrara@c2r1p1.42sp.org.br>
2024-09-20 17:01:51 -0300

    utils para o parser

--- v2/parser.c
+++ v2/parser.c
@@ -6,12 +6,66 @@
 /*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:10:17 by vcarrara          #+#    #+#             */
-/*   Updated: 2024/09/20 16:20:21 by vcarrara         ###   ########.fr       */
+/*   Updated: 2024/09/20 16:28:07 by vcarrara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/minishell.h"
 
+void	ft_parse_quotes(t_parse *parse, char *prompt, char *signal)
+{
+	signal = "";
+	if (ft_strchr("\"", parse->prompt[parse->idx->i]))
+	{
+		prompt[parse->idx->j++] = '\"';
+		signal = "\"";
+	}
+	else
+	{
+		prompt[parse->idx->j++] = '\'';
+		signal = "\'";
+	}
+	parse->idx->i++;
+	while (!ft_strchr(signal, parse->prompt[parse->idx->i]))
+	{
+		if (parse->prompt[parse->idx->i] == ' ')
+		{
+			prompt[parse->idx->j++] = 0x1A;
+			parse->idx->i++;
+		}
+		else
+			prompt[parse->idx->j++] = parse->prompt[parse->idx->i++];
+	}
+}
+
+int	ft_aux_parse_char(char *ch, t_parse *parse, char *prompt)
+{
+	if (ft_strncmp(ch, &parse->prompt[parse->idx->i], 2) == 0)
+	{
+		prompt[parse->idx->j++] = ' ';
+		prompt[parse->idx->j++] = parse->prompt[parse->idx->i++];
+		prompt[parse->idx->j++] = parse->prompt[parse->idx->i];
+		prompt[parse->idx->j] = ' ';
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_parse_char(t_parse *parse, char *prompt)
+{
+	if (ft_aux_parse_char("<<", parse, prompt))
+		return (1);
+	else if (ft_aux_parse_char(">>", parse, prompt))
+		return (1);
+	else if (ft_strchr("><|", parse->prompt[parse->idx->i]))
+	{
+		prompt[parse->idx->j++] = ' ';
+		prompt[parse->idx->j++] = parse->prompt[parse->idx->i];
+		prompt[parse->idx->j] = ' ';
+		return (1);
+	}
+	return (0);
+}
 void	ft_prompt_ordering(t_parse *parse) // TODO: refatorar essa função
 {
 	int		size;
